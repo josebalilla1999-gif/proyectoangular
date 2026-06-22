696e43bd348cfb03b081e2d7113bb1cd97073ae5
josebalilla1999-gif <josebalilla1999@gmail.com>
2026-06-22 12:33:51 +0200

    22-Jun v1.2

--- proyectoangular/src/app/app.routes.ts
+++ proyectoangular/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { PokemonComponent } from './pokemon/pokemon';
 import { PokemonDetailComponent } from './pokemondetail/pokemondetail';
 import { Quiz } from './quiz/quiz';
+import { Index } from './index';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: Index
+    },
     {
         path: 'pokemon',
         component: PokemonComponent
